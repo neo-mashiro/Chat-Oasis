be5f89794ad395dba71b1ed1e45b23f8a7f8a694
neo-mashiro <neo-mashiro@hotmail.com>
2020-10-13 17:48:31 -0400

    update fps scripts

---
+++ Scripts/LifespanManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// manage the lifespan of a game object
+public class LifespanManager : MonoBehaviour {
+
+	public float timeout = 1.0f;
+	public bool detachChildren = false;
+
+	private void Awake () {
+		Invoke (nameof(Suicide), timeout);
+	}
+
+	private void Suicide () {
+		if (detachChildren) {
+			transform.DetachChildren();
+		}
+		
+		Destroy(gameObject);
+	}
+}
